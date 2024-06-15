9dd903902cc2bb008d349c1346308e832fb57813
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-15 10:05:55 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,24 @@
 source_urls = [
-    'https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt', #每天自动更新1次
-    'https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v6.txt', #每天自动更新1次
-    'https://raw.githubusercontent.com/Guovin/TV/gd/result.txt', #每天自动更新1次
-    'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', #每天自动更新1次
-    'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',  #1小时自动更新1次11:11 2024/05/13
-    'https://raw.githubusercontent.com/fenxp/iptv/main/live/tvlive.txt', #1小时自动更新1次11:11 2024/05/13
-    'https://raw.githubusercontent.com/PizazzGY/TVBox_warehouse/main/live.txt',
-    'https://raw.githubusercontent.com/yuanzl77/IPTV/main/直播/ipv6.txt',
-    'https://raw.githubusercontent.com/Supprise0901/TVBox_live/main/live.txt',
-    'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
-    'https://raw.githubusercontent.com/gaotianliuyun/gao/master/list.txt',
+    'https://raw.github.com/ssili126/tv/main/itvlist.txt', #每天自动更新1次
+    'https://raw.github.com/fenxp/iptv/main/live/ipv6.txt',  #1小时自动更新1次11:11 2024/05/13
+    'https://raw.github.com/fenxp/iptv/main/live/tvlive.txt', #1小时自动更新1次11:11 2024/05/13
+    'https://raw.github.com/PizazzGY/TVBox_warehouse/main/live.txt',
+    'https://raw.github.com/yuanzl77/IPTV/main/直播/ipv6.txt',
+    'https://raw.github.com/Supprise0901/TVBox_live/main/live.txt',
+    'https://raw.github.com/mlvjfchen/TV/main/iptv_list.txt',
+    'https://raw.github.com/gaotianliuyun/gao/master/list.txt',
     'https://m3u.ibert.me/txt/fmml_ipv6.txt',
     'https://m3u.ibert.me/txt/fmml_dv6.txt',
     'https://m3u.ibert.me/txt/ycl_iptv.txt',
     'https://m3u.ibert.me/txt/y_g.txt',
     'https://m3u.ibert.me/txt/j_home.txt',
     'https://m3u.ibert.me/txt/j_iptv.txt',
+    'https://cdn.jsdelivr.net/gh/shidahuilang/shuyuan@shuyuan/iptv.txt',
     'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
     'https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt',
     'http://120.79.4.185/new/mdlive.txt',
     'https://tv.youdu.fan:666/live/',
-    'https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt',
-    'https://cdn.jsdelivr.net/gh/shidahuilang/shuyuan@shuyuan/iptv.txt'
+    'https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1715581924111/live1.txt'
 ]
 
 url_blacklist = [
