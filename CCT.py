3633fae75bf29c3bf866f9dcc65e732b84684d3c
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-17 03:07:02 +0800

    Update CCT.py

--- CCT.py
+++ CCT.py
@@ -3,7 +3,6 @@ import re
 import os
 from datetime import datetime
 
-# 定义要访问的多个URL
 urls = [
     'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', #每天自动更新1次
     'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',  #1小时自动更新1次
@@ -32,27 +31,27 @@ urls = [
 ]
 
 # 定义多个对象用于存储不同内容的行文本
-ys_lines = [] #央视频道
-ws_lines = [] #卫视频道
-ty_lines = [] #体育频道
-dy_lines = [] #电影
-dsj_lines = [] #电视剧
-gat_lines = [] #港澳台
-gj_lines = [] #国际台
-jlp_lines = [] #记录片
-dhp_lines = [] #动画片
-xq_lines = [] #戏曲
-js_lines = [] #解说
-cw_lines = [] #春晚
-mx_lines = [] #明星
-ztp_lines = [] #主题片
-zy_lines = [] #综艺频道
-yy_lines = [] #音乐频道
-game_lines = [] #游戏频道
-radio_lines = [] #收音机频道
-gd_lines = [] #地方台-广东频道
-hn_lines = [] #地方台-湖南频道
-other_lines = [] #其他频道
+ys_lines = []   #央视频道
+ws_lines = []   #卫视频道
+ty_lines = []   #体育频道
+dy_lines = []   #电影
+dsj_lines = []  #电视剧
+gat_lines = []  #港澳台
+gj_lines = []   #国际台
+jlp_lines = []  #记录片
+dhp_lines = []  #动画片
+xq_lines = []   #戏曲
+js_lines = []   #解说
+cw_lines = []   #春晚
+mx_lines = []   #明星
+ztp_lines = []  #主题片
+zy_lines = []   #综艺频道
+yy_lines = []   #音乐频道
+game_lines = []   #游戏频道
+radio_lines = []  #收音机频道
+gd_lines = []     #地方台-广东频道
+hn_lines = []     #地方台-湖南频道
+other_lines = []  #其他频道
 
 def process_name_string(input_str):
     parts = input_str.split(',')
@@ -63,27 +62,23 @@ def process_name_string(input_str):
     result_str = ','.join(processed_parts)
     return result_str
 
-def process_part(part_str):  # 处理逻辑
+def process_part(part_str):
     if "CCTV" in part_str and "://" not in part_str:
-        part_str = part_str.replace("IPV6", "")   #先剔除IPV6字样
+        part_str = part_str.replace("IPV6", "")
         part_str = part_str.replace("PLUS", "+")
         filtered_str = ''.join(char for char in part_str if char.isdigit() or char == 'K' or char == '+')
-        if not filtered_str.strip():  #处理特殊情况，如果发现没有找到频道数字返回原名称
+        if not filtered_str.strip():
             filtered_str = part_str.replace("CCTV", "")
 
-        if len(filtered_str) > 2 and re.search(r'4K|8K', filtered_str):  # 特殊处理CCTV中部分4K和8K名称
-          # 使用正则表达式替换，删除4K或8K后面的字符，并且保留4K或8K
+        if len(filtered_str) > 2 and re.search(r'4K|8K', filtered_str):
             filtered_str = re.sub(r'(4K|8K).*', r'\1', filtered_str)
             if len(filtered_str) > 2:
-                # 给4K或8K添加括号
                 filtered_str = re.sub(r'(4K|8K)', r'(\1)', filtered_str)
 
         return "CCTV" + filtered_str
 
-    elif "卫视" in part_str:  
-        # 定义正则表达式模式，匹配“卫视”后面的内容
+    elif "卫视" in part_str:
         pattern = r'卫视「.*」'
-        # 使用sub函数替换匹配的内容为空字符串
         result_str = re.sub(pattern, '卫视', part_str)
         return result_str
 
@@ -91,70 +86,64 @@ def process_part(part_str):  # 处理逻辑
 
 def process_url(url):
     try:
-        # 打开URL并读取内容
         with urllib.请求.urlopen(url) as response:
-            # 以二进制方式读取数据
             data = response.read()
-            # 将二进制数据解码为字符串
             text = data.decode('utf-8')
 
-            # 逐行处理内容
             lines = text.split('\n')
             for line in lines:
                 if "#genre#" not in line and "," in line and "://" in line:
                     channel_name = line.split(',')[0].strip()
                     channel_address = line.split(',')[1].strip()
-                    # 根据行内容判断存入哪个对象
                     
                     if "CCTV" in channel_name:
-                        ys_lines.append(process_name_string(line.strip()))   #央视频道
+                        ys_lines.append(process_name_string(line.strip()))
                     elif channel_name in ws_dictionary:
-                        ws_lines.append(process_name_string(line.strip()))   #卫视频道
+                        ws_lines.append(process_name_string(line.strip()))
                     elif channel_name in ty_dictionary:
-                        ty_lines.append(process_name_string(line.strip()))   #体育频道
+                        ty_lines.append(process_name_string(line.strip()))
                     elif channel_name in dy_dictionary:
-                        dy_lines.append(process_name_string(line.strip()))   #电影频道
+                        dy_lines.append(process_name_string(line.strip()))
                     elif channel_name in dsj_dictionary:
-                        dsj_lines.append(process_name_string(line.strip()))  #电视剧频道
+                        dsj_lines.append(process_name_string(line.strip()))
                     elif channel_name in gat_dictionary:
-                        gat_lines.append(process_name_string(line.strip()))  #港澳台
+                        gat_lines.append(process_name_string(line.strip()))
                     elif channel_name in gj_dictionary:
-                        gj_lines.append(process_name_string(line.strip()))   #国际台
+                        gj_lines.append(process_name_string(line.strip()))
                     elif channel_name in jlp_dictionary:
-                        jlp_lines.append(process_name_string(line.strip()))  #纪录片
+                        jlp_lines.append(process_name_string(line.strip()))
                     elif channel_name in dhp_dictionary:
-                        dhp_lines.append(process_name_string(line.strip()))  #动画片
+                        dhp_lines.append(process_name_string(line.strip()))
                     elif channel_name in xq_dictionary:
-                        xq_lines.append(process_name_string(line.strip()))   #戏曲
+                        xq_lines.append(process_name_string(line.strip()))
                     elif channel_name in js_dictionary:
-                        js_lines.append(process_name_string(line.strip()))   #解说
+                        js_lines.append(process_name_string(line.strip()))
                     elif channel_name in cw_dictionary:
-                        cw_lines.append(process_name_string(line.strip()))   #春晚
+                        cw_lines.append(process_name_string(line.strip()))
                     elif channel_name in mx_dictionary:
-                        mx_lines.append(process_name_string(line.strip()))   #明星
+                        mx_lines.append(process_name_string(line.strip()))
                     elif channel_name in ztp_dictionary:
-                        ztp_lines.append(process_name_string(line.strip()))  #主题片
+                        ztp_lines.append(process_name_string(line.strip()))
                     elif channel_name in zy_dictionary:
-                        zy_lines.append(process_name_string(line.strip()))   #综艺频道
+                        zy_lines.append(process_name_string(line.strip()))
                     elif channel_name in yy_dictionary:
-                        yy_lines.append(process_name_string(line.strip()))   #音乐频道
+                        yy_lines.append(process_name_string(line.strip()))
                     elif channel_name in game_dictionary:
-                        game_lines.append(process_name_string(line.strip())) #游戏频道
+                        game_lines.append(process_name_string(line.strip()))
                     elif channel_name in radio_dictionary:
-                        radio_lines.append(process_name_string(line.strip())) #收音机频道
+                        radio_lines.append(process_name_string(line.strip()))
                     elif channel_name in gd_dictionary:
-                        gd_lines.append(process_name_string(line.strip()))   #广东频道
+                        gd_lines.append(process_name_string(line.strip()))
                     elif channel_name in hn_dictionary:
-                        hn_lines.append(process_name_string(line.strip()))   #湖南频道
+                        hn_lines.append(process_name_string(line.strip()))
                     else:
                         other_lines.append(line.strip())
 
     except Exception as e:
         print(f"处理URL时发生错误：{e}")
 
-current_directory = os.getcwd()   #准备读取txt
+current_directory = os.getcwd()
 
-#读取文本方法
 def read_txt_to_array(file_name):
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
@@ -168,7 +157,6 @@ def read_txt_to_array(file_name):
         print(f"An error occurred: {e}")
         return []
 
-#读取文本   （过滤+排序）
 ys_dictionary = read_txt_to_array('CCTV.txt')
 ws_dictionary = read_txt_to_array('卫视频道.txt')
 ty_dictionary = read_txt_to_array('体育频道.txt')
@@ -190,7 +178,6 @@ radio_dictionary = read_txt_to_array('收音机频道.txt')
 gd_dictionary = read_txt_to_array('地方台/广东频道.txt')
 hn_dictionary = read_txt_to_array('地方台/湖南频道.txt')
 
-#读取纠错频道名称方法
 def load_corrections_name(filename):
     corrections = {}
     try:
@@ -206,10 +193,8 @@ def load_corrections_name(filename):
         print(f"An error occurred: {e}")
     return corrections
 
-#读取纠错文件
 corrections_name = load_corrections_name('corrections_name.txt')
 
-#纠错频道名称
 def correct_name_data(corrections, data):
     corrected_data = []
     for line in data:
@@ -220,15 +205,12 @@ def correct_name_data(corrections, data):
     return corrected_data
 
 def sort_data(order, data):
-    # 创建一个字典来存储每行数据的索引
     order_dict = {name: i for i, name in enumerate(order)}
-
-    # 定义一个排序键函数，处理不在 order_dict 中的字符串
+    
     def sort_key(line):
         name = line.split(',')[0]
         return order_dict.get(name, len(order))
-
-    # 按照 order 中的顺序对数据进行排序
+    
     sorted_data = sorted(data, key=sort_key)
     return sorted_data
 
@@ -242,7 +224,6 @@ def save_data(filename, data):
     except Exception as e:
         print(f"An error occurred while saving the file '{filename}': {e}")
 
-# 循环处理每个URL
 for url in urls:
     process_url(url)
 
