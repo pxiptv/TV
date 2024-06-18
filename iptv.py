f6eb02a0bb3e12e52fa3e24a3b484111d980ec9b
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-18 22:33:08 +0800

    Update iptv.py

--- iptv.py
+++ iptv.py
@@ -5,7 +5,21 @@ from datetime import datetime
 
 urls = [
     'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt',
-    'https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt'
+    'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt', 
+    'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',
+    'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
+    'https://raw.githubusercontent.com/maitel2020/iptv-self-use/main/iptv.txt',
+    'https://raw.githubusercontent.com/kimwang1978/collect-tv-txt/main/merged_output.txt',
+    'https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt',
+    'https://m3u.ibert.me/txt/fmml_ipv6.txt',
+    'https://m3u.ibert.me/txt/fmml_dv6.txt',
+    'https://m3u.ibert.me/txt/ycl_iptv.txt',
+    'https://m3u.ibert.me/txt/y_g.txt',
+    'https://m3u.ibert.me/txt/j_iptv.txt',
+    'https://live.zhoujie218.top/dsyy/mylist.txt',
+    'https://cdn.jsdelivr.net/gh/shidahuilang/shuyuan@shuyuan/iptv.txt',
+    'https://gitee.com/xxy002/zhiboyuan/raw/master/zby.txt',
+    'https://gitlab.com/p2v5/wangtv/-/raw/main/lunbo.txt'
 ]
 
 ys_lines = []
@@ -60,7 +74,7 @@ def process_part(part_str):
 
 def process_url(url):
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.请求.urlopen(url) as response:
             data = response.read()
             text = data.decode('utf-8')
 
