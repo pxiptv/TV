97d44071f6faad5b442f84f2441aa992f4d2020d
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-20 11:19:59 +0800

    Update qc.py

--- qc.py
+++ qc.py
@@ -7,22 +7,28 @@ timestart = datetime.now()
 
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        return set(file.readlines())
+        return file.readlines()
 
 def write_file(file_path, lines):
     with open(file_path, 'w', encoding='utf-8') as file:
         file.writelines(lines)
 
-def remove_blacklisted_lines(input_file, blacklist_file, output_file):
+def remove_blacklisted_lines(input_file, blacklist_file, output_file, header_lines_count=5):
     # 读取输入文件和黑名单文件
     input_lines = read_file(input_file)
-    blacklist_lines = read_file(blacklist_file)
+    blacklist_lines = set(read_file(blacklist_file))
 
-    # 计算差集
-    filtered_lines = input_lines - blacklist_lines
+    # 保留前header_lines_count行内容
+    header_lines = input_lines[:header_lines_count]
+
+    # 去除黑名单中的行
+    filtered_lines = [line for line in input_lines[header_lines_count:] if line not in blacklist_lines]
+
+    # 合并保留的前header_lines_count行和过滤后的内容
+    result_lines = header_lines + filtered_lines
 
     # 写入输出文件
-    write_file(output_file, filtered_lines)
+    write_file(output_file, result_lines)
 
 if __name__ == "__main__":
     input_file = 'merged_output.txt'
