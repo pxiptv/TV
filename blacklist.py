4b544e69f09bacc42bda2a03432f7dc52b378d12
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-21 16:35:25 +0800

    Update blacklist.py

--- blacklist.py
+++ blacklist.py
@@ -2,13 +2,16 @@ import urllib.request
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 from datetime import datetime
+import os
+from urllib.parse import urlparse
+
 
 timestart = datetime.now()
 
 # 读取文件内容
 def read_txt_file(file_path):
-    skip_strings = ['#genre#']  # 定义需要跳过的字符串数组
-    required_strings = ['://']  # 定义需要包含的字符串数组
+    skip_strings = ['#genre#']  # 定义需要跳过的字符串数组['#', '@', '#genre#'] 
+    required_strings = ['://']  # 定义需要包含的字符串数组['必需字符1', '必需字符2'] 
 
     with open(file_path, 'r', encoding='utf-8') as file:
         lines = [
@@ -23,7 +26,7 @@ headers = {
 }
 def check_url(url, timeout=8):
     try:
-        if "://" in url:
+    	if  "://" in url:
             start_time = time.time()
             req = urllib.request.Request(url, headers=headers)
             with urllib.request.urlopen(req, timeout=timeout) as response:
@@ -36,7 +39,7 @@ def check_url(url, timeout=8):
 
 # 处理单行文本并检测URL
 def process_line(line):
-    if "#genre#" in line or "://" not in line:
+    if "#genre#" in line or "://" not in line :
         return None, None  # 跳过包含“#genre#”的行
     parts = line.split(',')
     if len(parts) == 2:
@@ -49,8 +52,8 @@ def process_line(line):
     return None, None
 
 # 多线程处理文本并检测URL
-def process_urls_multithreaded(lines, max_workers=11):
-    blacklist = []
+def process_urls_multithreaded(lines, max_workers=18):
+    blacklist =  [] 
     successlist = []
 
     with ThreadPoolExecutor(max_workers=max_workers) as executor:
@@ -70,50 +73,157 @@ def write_list(file_path, data_list):
         for item in data_list:
             file.write(item + '\n')
 
+# 增加外部url到检测清单，同时支持检测m3u格式url
+# urls里所有的源都读到这里。
+urls_all_lines = []
+
+def get_url_file_extension(url):
+    # 解析URL
+    parsed_url = urlparse(url)
+    # 获取路径部分
+    path = parsed_url.path
+    # 提取文件扩展名
+    extension = os.path.splitext(path)[1]
+    return extension
+
+def convert_m3u_to_txt(m3u_content):
+    # 分行处理
+    lines = m3u_content.split('\n')
+    
+    # 用于存储结果的列表
+    txt_lines = []
+    
+    # 临时变量用于存储频道名称
+    channel_name = ""
+    
+    for line in lines:
+        # 过滤掉 #EXTM3U 开头的行
+        if line.startswith("#EXTM3U"):
+            continue
+        # 处理 #EXTINF 开头的行
+        if line.startswith("#EXTINF"):
+            # 获取频道名称（假设频道名称在引号后）
+            channel_name = line.split(',')[-1].strip()
+        # 处理 URL 行
+        elif line.startswith("http"):
+            txt_lines.append(f"{channel_name},{line.strip()}")
+    
+    # 将结果合并成一个字符串，以换行符分隔
+    return '\n'.join(txt_lines)
+
+def process_url(url):
+    try:
+        # 打开URL并读取内容
+        with urllib.request.urlopen(url) as response:
+            # 以二进制方式读取数据
+            data = response.read()
+            # 将二进制数据解码为字符串
+            text = data.decode('utf-8')
+            if get_url_file_extension(url)==".m3u" or get_url_file_extension(url)==".m3u8":
+                urls_all_lines.append(convert_m3u_to_txt(text))
+            elif get_url_file_extension(url)==".txt":
+                lines = text.split('\n')
+                for line in lines:
+                    if  "#genre#" not in line and "," in line and "://" in line:
+                        #channel_name=line.split(',')[0].strip()
+                        #channel_address=line.split(',')[1].strip()
+                        urls_all_lines.append(line.strip())
+    
+    except Exception as e:
+        print(f"处理URL时发生错误：{e}")
+
+
 if __name__ == "__main__":
+    # 定义要访问的多个URL
+    urls = [
+        'https://raw.bgithub.xyz/YanG-1989/m3u/main/Gather.m3u',
+        'https://raw.bgithub.xyz/iptv-org/iptv/master/streams/cn.m3u',
+        'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt'
+    ]
+    for url in urls:
+        print(f"处理URL: {url}")
+        process_url(url)   #读取上面url清单中直播源存入urls_all_lines
+
     input_file1 = 'iptv.txt'  # 输入文件路径
     input_file2 = 'blacklist_auto.txt'  # 输入文件路径2 
     success_file = 'whitelist_auto.txt'  # 成功清单文件路径
+    success_file_tv = 'whitelist_auto_tv.txt'  # 成功清单文件路径（另存一份直接引用源）
     blacklist_file = 'blacklist_auto.txt'  # 黑名单文件路径
 
-# 读取输入文件内容
+    # 读取输入文件内容
     lines1 = read_txt_file(input_file1)
     lines2 = read_txt_file(input_file2)
-    lines = list(set(lines1 + lines2))
-# 计算合并后合计个数
+    lines=list(set(urls_all_lines + lines1 + lines2))
+    # 计算合并后合计个数
     urls_hj = len(lines)
 
-# 处理URL并生成成功清单和黑名单
+    # 处理URL并生成成功清单和黑名单
     successlist, blacklist = process_urls_multithreaded(lines)
     
-# 给successlist, blacklist排序
-# 定义排序函数
-def successlist_sort_key(item):
-    time_str = item.split(',')[0].replace('ms', '')
-    return float(time_str)
+    # 给successlist, blacklist排序
+    # 定义排序函数
+    def successlist_sort_key(item):
+        time_str = item.split(',')[0].replace('ms', '')
+        return float(time_str)
     
-    successlist = sorted(successlist, key=successlist_sort_key)
-    blacklist = sorted(blacklist)
+    successlist=sorted(successlist, key=successlist_sort_key)
+    blacklist=sorted(blacklist)
 
-# 计算check后ok和ng个数
+    # 计算check后ok和ng个数
     urls_ok = len(successlist)
     urls_ng = len(blacklist)
 
-# 加时间戳等
-    version = datetime.now().strftime("%Y%m%d-%H-%M-%S") + ",url"
-    successlist = ["更新时间,#genre#"] + [version] + ['\n'] + \
+    # 把successlist整理一下，生成一个可以直接引用的源，方便用zyplayer手动check
+    def remove_prefix_from_lines(lines):
+        result = []
+        for line in lines:
+            if  "#genre#" not in line and "," in line and "://" in line:
+                parts = line.split(",")
+                result.append(",".join(parts[1:]))
+        return result
+
+
+    # 加时间戳等
+    version=datetime.now().strftime("%Y%m%d-%H-%M-%S")+",url"
+    successlist_tv = ["更新时间,#genre#"] +[version] + ['\n'] +\
+                  ["whitelist,#genre#"] + remove_prefix_from_lines(successlist)
+    successlist = ["更新时间,#genre#"] +[version] + ['\n'] +\
                   ["RespoTime,whitelist,#genre#"] + successlist
-    blacklist = ["更新时间,#genre#"] + [version] + ['\n'] + \
-                ["blacklist,#genre#"] + blacklist
+    blacklist = ["更新时间,#genre#"] +[version] + ['\n'] +\
+                ["blacklist,#genre#"]  + blacklist
 
-# 写入成功清单文件
+    # 写入成功清单文件
     write_list(success_file, successlist)
+    write_list(success_file_tv, successlist_tv)
 
-# 写入黑名单文件
+    # 写入黑名单文件
     write_list(blacklist_file, blacklist)
 
     print(f"成功清单文件已生成: {success_file}")
-    print(f"黑名单文件已生成: {blacklist}")
+    print(f"成功清单文件已生成(tv): {success_file_tv}")
+    print(f"黑名单文件已生成: {blacklist_file}")
+
+    # 执行的代码
+    timeend = datetime.now()
+
+    # 计算时间差
+    elapsed_time = timeend - timestart
+    total_seconds = elapsed_time.total_seconds()
+
+    # 转换为分钟和秒
+    minutes = int(total_seconds // 60)
+    seconds = int(total_seconds % 60)
+
+    # 格式化开始和结束时间
+    timestart_str = timestart.strftime("%Y%m%d_%H_%M_%S")
+    timeend_str = timeend.strftime("%Y%m%d_%H_%M_%S")
+
+    print(f"开始时间: {timestart_str}")
+    print(f"结束时间: {timeend_str}")
+    print(f"执行时间: {minutes} 分 {seconds} 秒")
+    print(f"urls_hj: {urls_hj} ")
+    print(f"  urls_ok: {urls_ok} ")
+    print(f"  urls_ng: {urls_ng} ")
     
 ################# 添加生成m3u文件
     output_text = "#EXTM3U\n"
@@ -135,25 +245,3 @@ def successlist_sort_key(item):
         file.write(output_text)
 
     print("iptv.m3u文件已生成。")
-
-# 执行的代码
-    timeend = datetime.now()
-
-# 计算时间差
-    elapsed_time = timeend - timestart
-    total_seconds = elapsed_time.total_seconds()
-
-# 转换为分钟和秒
-    minutes = int(total_seconds // 60)
-    seconds = int(total_seconds % 60)
-
-# 格式化开始和结束时间
-    timestart_str = timestart.strftime("%Y%m%d_%H_%M_%S")
-    timeend_str = timeend.strftime("%Y%m%d_%H_%M_%S")
-
-    print(f"开始时间: {timestart_str}")
-    print(f"结束时间: {timeend_str}")
-    print(f"执行时间: {minutes} 分 {seconds} 秒")
-    print(f"urls_hj: {urls_hj} ")
-    print(f"  urls_ok: {urls_ok} ")
-    print(f"  urls_ng: {urls_ng} ")
