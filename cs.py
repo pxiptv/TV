74d1255679ddcfa0504e9a429e3abf9d6e6580a8
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-23 11:50:02 +0800

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,5 +1,32 @@
+import urllib.request
 import os
+import requests
 
+# 从URL获取文件内容
+def fetch_content_from_url(url):
+    try:
+        with urllib.request.urlopen(url) as response:
+            return response.read().decode('utf-8')
+    except Exception as e:
+        print(f"Error fetching {url}: {e}")
+        return ""
+
+# 处理并过滤URL内容
+def process_content(content):
+    skip_strings = ['#genre#', '192', '198', 'ChiSheng9']
+    lines = content.split('\n')
+    filtered_lines = [line for line in lines if not any(skip in line for skip in skip_strings)]
+    return filtered_lines
+    
+# 从URL列表下载和处理文件内容
+def download_and_process_files(urls):
+    all_lines = set()
+    for url in urls:
+        content = fetch_content_from_url(url)
+        if content:
+            all_lines.update(process_content(content))
+    return list(all_lines)
+    
 # 读取文件内容
 def read_txt_file(file_path):
     if os.path.exists(file_path):
@@ -14,9 +41,22 @@ def write_txt_file(file_path, lines):
         file.write('\n'.join(lines) + '\n')
 
 # 主函数
+if __name__ == "__main__":
+    urls = [
+        'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt',
+        'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',
+        'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',
+        'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
+        'https://raw.githubusercontent.com/maitel2020/iptv-self-use/main/iptv.txt',
+        'https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt',
+        'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt'
+    ]
+
+    # 下载并处理所有URL的内容
+    all_lines = download_and_process_files(urls)
+    
 def main():
-    # 读取 live.txt, iptv.txt, blacklist.txt 和 others.txt 文件
-    live_lines = read_txt_file('live.txt')
+    # 读取 iptv.txt, blacklist.txt 和 others.txt 文件
     iptv_lines = read_txt_file('iptv.txt')
     blacklist_lines = read_txt_file('blacklist.txt')
     others_lines = read_txt_file('others.txt')
@@ -25,7 +65,7 @@ def main():
     combined_lines = set(iptv_lines + blacklist_lines + others_lines)
 
     # 过滤 live.txt 中的重复行
-    filtered_live_lines = [line for line in live_lines if line and line not in combined_lines]
+    filtered_live_lines = [line for line in all_lines if line and line not in combined_lines]
 
     # 写入去重后的 live.txt 文件
     write_txt_file('live.txt', filtered_live_lines)
