8291bc5dfcc7f1109f6fbe0a10cc3c9153615b20
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-24 07:15:16 +0800

    Update cs.py

--- cs.py
+++ cs.py
@@ -1,6 +1,12 @@
 import urllib.request
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import time
+from datetime import datetime
 import os
-import requests
+from urllib.parse import urlparse
+
+
+timestart = datetime.now()
 
 # 从URL获取文件内容
 def fetch_content_from_url(url):
@@ -35,6 +41,14 @@ def append_to_file(file_path, lines):
     with open(file_path, 'a', encoding='utf-8') as file:
         for line in lines:
             file.write(line + '\n')
+# 合并两个文件的内容并写入输出文件
+def merge_files(file1, file2, output_file):
+    lines1 = read_txt_file(file1)
+    lines2 = read_txt_file(file2)
+
+    # 合并并去重
+    merged_lines = list(set(lines1 + lines2))
+    write_txt_file(output_file, merged_lines)
 
 # 删除重复行
 def remove_duplicates(lines, file_paths):
@@ -42,7 +56,20 @@ def remove_duplicates(lines, file_paths):
         file_lines = read_txt_file(file_path)
         lines = [line for line in lines if line not in file_lines]
     return lines
+    
+# 过滤掉在 comparison_files 中出现的行
+def filter_lines(input_file, comparison_files):
+    # 读取对比文件中的所有行
+    comparison_lines = set()
+    for file in comparison_files:
+        comparison_lines.update(read_txt_file(file))
 
+    # 读取输入文件并过滤行
+    input_lines = read_txt_file(input_file)
+    filtered_lines = [line for line in input_lines if line not in comparison_lines]
+    
+    return filtered_lines
+    
 # 从URL列表下载和处理文件内容
 def download_and_process_files(urls):
     all_lines = set()
@@ -52,15 +79,52 @@ def download_and_process_files(urls):
             all_lines.update(process_content(content))
     return list(all_lines)
 
-# 在线检测URL是否可访问
-def check_url_availability(url):
+# 检测URL是否可访问并记录响应时间
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+}
+def check_url(url, timeout=8):
     try:
-        response = requests.get(url, timeout=10)
-        if response.status_code == 200:
-            return True, response.elapsed.total_seconds() * 1000
-    except requests.RequestException:
-        pass
-    return False, 0
+    	if  "://" in url:
+            start_time = time.time()
+            req = urllib.request.Request(url, headers=headers)
+            with urllib.request.urlopen(req, timeout=timeout) as response:
+                elapsed_time = (time.time() - start_time) * 1000  # 转换为毫秒
+                if response.status == 200:
+                    return elapsed_time, True
+    except Exception as e:
+        print(f"Error checking {url}: {e}")
+    return None, False
+
+# 处理单行文本并检测URL
+def process_line(line):
+    if "#genre#" in line or "://" not in line :
+        return None, None  # 跳过包含“#genre#”的行
+    parts = line.split(',')
+    if len(parts) == 2:
+        name, url = parts
+        elapsed_time, is_valid = check_url(url.strip())
+        if is_valid:
+            return elapsed_time, line.strip()
+        else:
+            return None, line.strip()
+    return None, None
+
+# 多线程处理文本并检测URL
+def process_urls_multithreaded(lines, max_workers=18):
+    blacklist =  [] 
+    successlist = []
+
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        futures = {executor.submit(process_line, line): line for line in lines}
+        for future in as_completed(futures):
+            elapsed_time, result = future.result()
+            if result:
+                if elapsed_time is not None:
+                    successlist.append(f"{elapsed_time:.2f}ms,{result}")
+                else:
+                    blacklist.append(result)
+    return successlist, blacklist
 
 # 将iptv.txt转换为iptv.m3u文件
 def convert_to_m3u(iptv_file, m3u_file):
@@ -73,40 +137,110 @@ def convert_to_m3u(iptv_file, m3u_file):
                 file.write(f"#EXTINF:-1,{parts[0]}\n")
                 file.write(f"{parts[1]}\n")
 
-# 主函数
+# 增加外部url到检测清单，同时支持检测m3u格式url
+# urls里所有的源都读到这里。
+urls_all_lines = []
+
+def get_url_file_extension(url):
+    # 解析URL
+    parsed_url = urlparse(url)
+    # 获取路径部分
+    path = parsed_url.path
+    # 提取文件扩展名
+    extension = os.path.splitext(path)[1]
+    return extension
+
+def convert_m3u_to_txt(m3u_content):
+    # 分行处理
+    lines = m3u_content.split('\n')
+    
+    # 用于存储结果的列表
+    txt_lines = []
+    
+    # 临时变量用于存储频道名称
+    channel_name = ""
+    
+    for line in lines:
+        # 过滤掉 #EXTM3U 开头的行
+        if line.startswith("#EXTM3U"):
+            continue
+        # 处理 #EXTINF 开头的行
+        if line.startswith("#EXTINF"):
+            # 获取频道名称（假设频道名称在引号后）
+            channel_name = line.split(',')[-1].strip()
+        # 处理 URL 行
+        elif line.startswith("http"):
+            txt_lines.append(f"{channel_name},{line.strip()}")
+    
+    # 将结果合并成一个字符串，以换行符分隔
+    return '\n'.join(txt_lines)
+
+def process_url(url):
+    try:
+        # 打开URL并读取内容
+        with urllib.request.urlopen(url) as response:
+            # 以二进制方式读取数据
+            data = response.read()
+            # 将二进制数据解码为字符串
+            text = data.decode('utf-8')
+            if get_url_file_extension(url)==".m3u" or get_url_file_extension(url)==".m3u8":
+                urls_all_lines.append(convert_m3u_to_txt(text))
+            elif get_url_file_extension(url)==".txt":
+                lines = text.split('\n')
+                for line in lines:
+                    if  "#genre#" not in line and "," in line and "://" in line:
+                        #channel_name=line.split(',')[0].strip()
+                        #channel_address=line.split(',')[1].strip()
+                        urls_all_lines.append(line.strip())
+    
+    except Exception as e:
+        print(f"处理URL时发生错误：{e}")
+
 if __name__ == "__main__":
+    # 定义要访问的多个URL
     urls = [
-        'https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt',
-        'https://raw.githubusercontent.com/fenxp/iptv/main/live/ipv6.txt',
-        'https://raw.githubusercontent.com/yuanzl77/IPTV/main/live.txt',
-        'https://raw.githubusercontent.com/mlvjfchen/TV/main/iptv_list.txt',
-        'https://raw.githubusercontent.com/maitel2020/iptv-self-use/main/iptv.txt',
-        'https://raw.githubusercontent.com/zwc456baby/iptv_alive/master/live.txt',
-        'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt'
+        'https://raw.githubusercontent.com/YanG-1989/m3u/main/Gather.m3u',
+        'https://raw.githubusercontent.com/iptv-org/iptv/master/streams/cn.m3u',
+        'https://gitlab.com/p2v5/wangtv/-/raw/main/wang-tvlive.txt',
+        'https://raw.githubusercontent.com/kimwang1978/tvbox/main/%E5%A4%A9%E5%A4%A9%E5%BC%80%E5%BF%83/lives/%E2%91%AD%E5%BC%80%E5%BF%83%E7%BA%BF%E8%B7%AF.txt'
     ]
+    for url in urls:
+        print(f"处理URL: {url}")
+        process_url(url)   #读取上面url清单中直播源存入urls_all_lines
 
-    # 1. 下载并处理所有URL的内容
-    all_lines = download_and_process_files(urls)
+        # 写入 online.txt 文件
+    write_txt_file('online.txt',urls_all_lines)
+    online_file = 'online.txt'
+    
+    # 合并 iptv.txt, blacklist.txt 和 others.txt 的所有行
+    comparison_files = ['iptv.txt', 'blacklist.txt', 'others.txt']
 
-    # 写入 live.txt 文件
-    write_txt_file('live.txt', all_lines)
+    # 过滤 live.txt 中的重复行
+    filtered_live_lines = filter_lines(online_file, comparison_files)
 
-    # 读取 live.txt 文件并删除与 iptv.txt, blacklist.txt 和 others.txt 中相同内容的行
-    live_lines = read_txt_file('live.txt')
-    live_lines = remove_duplicates(live_lines, ['iptv.txt', 'blacklist.txt', 'others.txt'])
+    # 清空 live.txt 文件
+    open('live.txt', 'w').close()
 
-    # 写入过滤后的 live.txt 文件
-    write_txt_file('live.txt', live_lines)
+    # 过滤后写入 live.txt 文件
+    write_txt_file('live.txt', filtered_live_lines)
 
-    # 读取 live.txt 和 whitelist.txt，合并去重，删除不包含 "://" 的行，生成 tv.txt 文件
-    whitelist_lines = read_txt_file('whitelist.txt')
-    combined_lines = set(live_lines + whitelist_lines)
-    tv_lines = [line for line in combined_lines if '://' in line]
-    write_txt_file('tv.txt', tv_lines)
+# 将 live.txt 与 whitelist.txt 合并
+    input_file1 = 'live.txt'  # 输入文件路径
+    input_file2 = 'whitelist.txt'  # 输入文件路径2 
+    success_file = 'whitelist.txt'  # 成功清单文件路径
+    blacklist_file = 'blacklist.txt'  # 黑名单文件路径
+
+    # 读取输入文件内容
+    lines1 = read_txt_file(input_file1)
+    lines2 = read_txt_file(input_file2)
+    lines=list(set(lines1 + lines2))
+    lines = [line.strip() for line in lines if line.strip()]
+    write_txt_file('live.txt',lines)
 
     # 2. 清空 iptv.txt 文件后读取 channel.txt 文件
     open('iptv.txt', 'w').close()
     channel_lines = read_txt_file('channel.txt')
+    tv_lines = read_txt_file('live.txt')
 
     # 处理 channel.txt 文件中的每一行
     for channel_line in channel_lines:
