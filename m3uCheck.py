7117519743a52e92c63994cfd5d3f05fc634155f
pxiptv <108559455+pxiptv@users.noreply.github.com>
2024-06-29 18:37:21 +0800

    Update m3uCheck.py

--- m3uCheck.py
+++ m3uCheck.py
@@ -2,138 +2,11 @@ import concurrent.futures
 import requests
 import time
 
-# 读取文件内容
-def read_txt_file(file_path):
-    skip_strings = ['#genre#', '127.0.0.1', '192.168', '198.168', 'ChiSheng9', 'epg.pw', 'p3p', '/udp/', '(576p)', '(540p)', '(360p)', '(480p)', '(180p)', '(404p)', 'r.jdshipin', 'generationnexxxt', 'live.goodiptv.club', 'playtv-live.ifeng']  # 定义需要跳过的字符串数组['#', '@', '#genre#'] 
-    required_strings = ['://']  # 定义需要包含的字符串数组['必需字符1', '必需字符2'] 
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        lines = [
-            line for line in file
-            if not any(skip_str in line for skip_str in skip_strings) and all(req_str in line for req_str in required_strings)
-        ]
-    return lines
-
-def read_txt(filename):
-    with open(filename, 'r', encoding='utf-8') as f:
-        return f.readlines()
-
-def append_to_file(filename, lines):
-    with open(filename, 'a', encoding='utf-8') as f:
-        for line in lines:
-            f.write(line)
-            
-# 格式化频道名称
-def process_name_string(input_str):
-    parts = input_str.split(',')
-    processed_parts = []
-    for part in parts:
-        processed_part = process_part(part)
-        processed_parts.append(processed_part)
-    result_str = ','.join(processed_parts)
-    return result_str
-
-def process_part(part_str):
-    # 处理逻辑
-    part_str = part_str.replace("「IPV6」", "")  # 剔除 「IPV6」
-    part_str = part_str.replace("IPV6", "")  # 剔除 IPV6
-    part_str = part_str.replace("「IPV4」", "")  # 剔除 「IPV4」
-    part_str = part_str.replace("IPV4", "")  # 剔除 IPV4 
-    part_str = part_str.replace("[V4]", "")  # 剔除 [V4]
-    part_str = part_str.replace("[V6]", "")  # 剔除 [V6]
-    part_str = part_str.replace("台,http", ",http")  # 替换 台
-    part_str = part_str.replace("高清,http", ",http")  # 替换 高清
-    part_str = part_str.replace("标清,http", ",http")  # 替换 标清  
-    part_str = part_str.replace("視", "视")  # 替换
-    part_str = part_str.replace("聞", "闻")  # 替换
-    part_str = part_str.replace("衛", "卫")  # 替换
-    part_str = part_str.replace("東", "东")  # 替换
-    part_str = part_str.replace("華", "华")  # 替换
-    part_str = part_str.replace("電", "电")  # 替换
-    part_str = part_str.replace("語", "语")  # 替换
-    part_str = part_str.replace("間", "间")  # 替换
-    part_str = part_str.replace("亞", "亚")  # 替换
-    part_str = part_str.replace("線", "线")  # 替换
-    part_str = part_str.replace("國", "国")  # 替换
-    part_str = part_str.replace("灣", "湾")  # 替换
-    part_str = part_str.replace("環", "环")  # 替换
-    part_str = part_str.replace("蓮", "莲")  # 替换
-    part_str = part_str.replace("鏡", "镜")  # 替换
-    part_str = part_str.replace("財經", "财经")  # 替换
-    part_str = part_str.replace("凤凰-", "凤凰")  # 替换
-    part_str = part_str.replace("鳳凰", "凤凰")  # 替换
-    part_str = part_str.replace("資訊", "资讯")  # 替换
-    part_str = part_str.replace("紀實", "纪实")  # 替换
-    part_str = part_str.replace(" HD", "")  # 剔除 HD
-    part_str = part_str.replace("HD", "")  # 剔除 HD
-    part_str = part_str.replace("𝟘", "0")  # 替换 𝟘
-    part_str = part_str.replace("𝟙", "1")  # 替换 𝟙
-    part_str = part_str.replace("𝟚", "2")  # 替换 𝟚
-    part_str = part_str.replace("𝟛", "3")  # 替换 𝟛
-    part_str = part_str.replace("𝟜", "4")  # 替换 𝟜
-    part_str = part_str.replace("𝟝", "5")  # 替换 𝟝
-    part_str = part_str.replace("𝟞", "6")  # 替换 𝟞
-    part_str = part_str.replace("𝟟", "7")  # 替换 𝟟
-    part_str = part_str.replace("𝟠", "8")  # 替换 𝟠
-    part_str = part_str.replace("𝟡", "9")  # 替换 𝟡
-    part_str = part_str.replace("移动咪咕直播", "咪咕体育")  # 替换 移动咪咕直播
-    part_str = part_str.replace("咪咕直播", "咪咕体育")  # 替换 咪咕直播
-    part_str = part_str.replace("咪咕视频", "咪咕体育")  # 替换 咪咕视频
-    part_str = part_str.replace("咪咕体育-", "咪咕体育")  # 替换 咪咕体育
-    part_str = part_str.replace("咪咕体育_", "咪咕体育")  # 替换 咪咕体育
-    part_str = part_str.replace("•", "")  # 先剔除 •  
-    part_str = part_str.replace("_4M1080HEVC", "")  # 剔除
-    part_str = part_str.replace("_2.5M1080HEVC", "")  # 剔除
-    part_str = part_str.replace(" (1080p)", "")  # 替换 1080p
-    part_str = part_str.replace(" (900p)", "")  # 替换 900p
-    part_str = part_str.replace(" (720p)", "")  # 替换 720p
-    part_str = part_str.replace(" (576p)", "")  # 替换 576p
-    part_str = part_str.replace(" (540p)", "")  # 替换 540p
-    part_str = part_str.replace(" (480p)", "")  # 替换 480p
-    part_str = part_str.replace(" (360p)", "")  # 替换 360p
-    part_str = part_str.replace(" (240p)", "")  # 替换 240p
-    part_str = part_str.replace(" (180p)", "")  # 替换 180p
-    part_str = part_str.replace("  [Geo-blocked]", "")  # 替换[Geo-blocked]
-    
-    if "CCTV" in part_str and "://" not in part_str:
-        part_str = part_str.replace("PLUS", "+")  # 替换 PLUS
-        part_str = part_str.replace("1080", "")  # 替换 1080
-        filtered_str = ''.join(char for char in part_str if char.isdigit() or char == 'K' or char == '+')
-        if not filtered_str.strip():  # 处理特殊情况，如果发现没有找到频道数字返回原名称
-            filtered_str = part_str.replace("CCTV", "")
-        if len(filtered_str) > 2 and re.search(r'4K|8K', filtered_str):  # 特殊处理CCTV中部分4K和8K名称
-            # 使用正则表达式替换，删除4K或8K后面的字符，并且保留4K或8K
-            filtered_str = re.sub(r'(4K|8K).*', r'\1', filtered_str)
-            if len(filtered_str) > 2: 
-                # 给4K或8K添加括号
-                filtered_str = re.sub(r'(4K|8K)', r'(\1)', filtered_str)
-        return "CCTV" + filtered_str 
-    elif "卫视" in part_str:
-        part_str = part_str.replace("-卫视", "卫视")  # 替换 -卫视
-        # 定义正则表达式模式，匹配“卫视”后面的内容
-        pattern = r'卫视「.*」'
-        # 使用sub函数替换匹配的内容为空字符串
-        result_str = re.sub(pattern, '卫视', part_str)
-        return result_str
-    return part_str
-
-def filter_and_save_channel_names(input_file):
-    with open(input_file, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
-    
-    processed_lines = []
-    for line in lines:
-        if ',' in line:
-            channel_name, url = line.split(',', 1)
-            processed_channel_name = process_name_string(channel_name)
-            processed_line = f"{processed_channel_name},{url}"
-            processed_lines.append(processed_line)
-        else:
-            processed_lines.append(line)
-    
-    with open(input_file, 'w', encoding='utf-8') as out_file:
-        for line in processed_lines:
-            out_file.write(line)
+# 读取IPTV网址列表
+def read_urls(file_path):
+    with open(file_path, 'r') as file:
+        urls = file.readlines()
+    return [url.strip().split(',')[1] for url in urls]
 
 # 检测单个URL的响应时间
 def check_url(url):
@@ -153,13 +26,12 @@ def detect_urls(urls):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         future_to_url = {executor.submit(check_url, url): url for url in urls}
         for future in concurrent.futures.as_completed(future_to_url):
-            url = future_to_url[future]
             try:
                 url, response_time = future.result()
                 if response_time is not None:
                     results.append((url, response_time))
             except Exception as e:
-                print(f"URL {url} generated an exception: {e}")
+                print(f"URL {future_to_url[future]} generated an exception: {e}")
     return results
 
 # 将响应速度最快的URL写入文件
@@ -174,12 +46,9 @@ def write_best_url(results, file_path):
 
 # 主函数
 def main():
-    iptv_file = 'iptv.txt'
-    filter_and_save_channel_names(iptv_file)
-    urls = read_txt_file('iptv.txt')
-        
+    urls = read_urls('iptv.txt')
     results = detect_urls(urls)
-    write_best_url(results, 'others.txt')
+    write_best_url(results, 'live.txt')
 
 if __name__ == "__main__":
     main()
